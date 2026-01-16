f2be4fe9be9e3062ad9cbfe1124a59692d6e072c
Nico <nicolas.spinoza30@gmail.com>
2026-01-16 19:51:45 -0300

    Append Configure session with flask session and  i try to do structure basic with layout.html.It is doing hard

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 
+import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+from cachelib.file import FileSystemCache
 
 
 # Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem
+
+app.config["SESSION_TYPE"] = "cachelib"
+app.config['SESSION_SERIALIZATION_FORMAT'] = 'json'
+app.config["SESSION_PERMANENT"] = False #Deleted when the browser is closed
+
+Session(app)
 
 #Time now
 #now = datetime.now()
